ebb711d9b560c5ce2eaaa0f98ed851a833194a78
[dyad] <wpwscannerfeed@gmail.com>
2025-09-26 16:17:18 -0400

    [dyad] Cleaned up unused and duplicate files - wrote 1 file(s), deleted 9 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -38,7 +37,7 @@ const ProtectedRoute = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+      {/* Removed Toaster component as sonner is now the primary toast system */}
       <Sonner />
       <BrowserRouter>
         <Routes>
