8e0a53ccb076af7e8ba2c0703ed8a76e7b12558a
[dyad] <wpwscannerfeed@gmail.com>
2025-09-30 23:26:54 -0400

    [dyad] Added `import "node:util";` to the `send-push-notification` Edge Function to resolve a Node.js compatibility error with the `web-push` library. - wrote 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/send-push-notification/index.ts
+++ supabase/functions/send-push-notification/index.ts
@@ -1,4 +1,8 @@
 // @ts-ignore
+/// <reference lib="deno.ns" />
+// @ts-ignore
+import "node:util"; // Explicitly import node:util for web-push compatibility
+// @ts-ignore
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
