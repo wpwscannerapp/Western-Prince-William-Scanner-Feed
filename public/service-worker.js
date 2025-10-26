8e16bdbd93c860044c93ad43ae7ec58d4f219c3a
[dyad] <wpwscannerfeed@gmail.com>
2025-10-26 00:08:28 -0400

    [dyad] Created a placeholder `public/service-worker.js` file to prevent MIME type errors and reiterated clearing browser site data. - wrote 1 file(s) + extra files edited outside of Dyad

---
+++ public/service-worker.js
@@ -0,0 +1,4 @@
+// This is a placeholder service worker file.
+// In development, vite-plugin-pwa typically uses 'dev-sw.js'.
+// This file ensures that if 'service-worker.js' is requested, it receives a valid JS file.
+console.log('Placeholder service-worker.js loaded.');
