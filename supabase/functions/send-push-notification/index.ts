7ea89782796b65c109ac0aa8744d60872178f1b8
[dyad] <wpwscannerfeed@gmail.com>
2025-11-17 15:04:23 -0500

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/send-push-notification/index.ts
+++ supabase/functions/send-push-notification/index.ts
@@ -5,7 +5,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
 // @ts-ignore
-import webpush from 'https://esm.sh/web-push@3.6.7'; // Import web-push
+import webpush from 'https://esm.sh/web-push@3.6.7?target=deno'; // Explicitly target Deno
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
