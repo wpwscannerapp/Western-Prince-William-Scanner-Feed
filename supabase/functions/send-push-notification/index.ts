d49779c70f04204d17969cff95f1d9e9b409392e
[dyad] <wpwscannerfeed@gmail.com>
2025-11-18 18:48:58 -0500

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/send-push-notification/index.ts
+++ supabase/functions/send-push-notification/index.ts
@@ -5,7 +5,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
 // @ts-ignore
-import webpush from 'https://esm.sh/web-push@3.7.0?target=deno'; // Updated web-push version
+import webpush from 'https://esm.sh/web-push@3.7.0'; // Removed ?target=deno
 
 // Explicitly declare Deno global for TypeScript
 declare const Deno: {
