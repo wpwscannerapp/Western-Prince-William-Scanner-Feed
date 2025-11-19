f78e2e2072ba8cbe66d6ddcb90d935cdb1fcdabf
[dyad] <wpwscannerfeed@gmail.com>
2025-11-18 23:34:45 -0500

    [dyad] Executed a test SQL insert into the alerts table to trigger push notifications. - wrote 1 file(s), executed 1 SQL queries + extra files edited outside of Dyad

---
+++ supabase/migrations/0290_inserting_a_sample_alert_to_test_push_notifications.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.alerts (title, description, type, latitude, longitude)
+VALUES (
+  'Test Alert from Dyad',
+  'This is a test notification to verify the Web Push functionality. Expect a push notification on subscribed devices!',
+  'Test',
+  38.8048,
+  -77.4731
+);
